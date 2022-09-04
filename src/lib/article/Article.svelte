3f5ac480a919bfd4ae6a74356a3458297d957971
netnetaip <netnetaip@gmail.com>
2022-09-04 13:05:06 +0300

    Styling changes

--- src/lib/article/Article.svelte
+++ src/lib/article/Article.svelte
@@ -62,7 +62,7 @@
 		</figure>
 		<p>{entry.text}</p>
 		<button type="button">
-			<svg width="30" height="30" stroke-width="1.8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M14 14H2M10 10H2M6 6H2M18 18H2M19 14V4m0 0l3 3m-3-3l-3 3" stroke="#000000" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+			<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M14 14H2M10 10H2M6 6H2M18 18H2M19 14V4m0 0l3 3m-3-3l-3 3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></path></svg>
 			{entry.rating}
 		</button>
 	</article>
